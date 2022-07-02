f7c8bc2ca4acbe42536acde0adb8418cb0751898
Krishvy <krishvyx2@gmail.com>
2022-07-02 18:20:14 +0530

    a

--- src/Krishvy/SimpleRepair/SimpleRepair.php
+++ src/Krishvy/SimpleRepair/SimpleRepair.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Krishvy;
+namespace Krishvy\SimpleRepair;
 
 use pocketmine\plugin\PluginBase;
-use Krishvy\RepairCommand;
+use Krishvy\SimpleRepair\RepairCommand;
 
 class SimpleRepair extends PluginBase
 {
